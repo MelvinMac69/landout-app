f046e91aead242cbb5a3d6062f4f5962bceac11f
Melvin <managermelvin69@gmail.com>
2026-04-06 14:24:46 -0700

    Slim mobile header: LANDOUT branding + tagline, no nav links

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components/layout/Header';
+import { Header, MobileHeader } from '@/components/layout/Header';
 import { MobileNav } from '@/components/layout/MobileNav';
 
 export default function MainLayout({
@@ -9,6 +9,7 @@ export default function MainLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
+      <MobileHeader />
       <main className="flex-1 pb-20 md:pb-0">{children}</main>
       <MobileNav />
     </div>
